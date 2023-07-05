bcb490609e5518213891f3fb92c815cb14926c52
Shritesh Bhattarai <shr@ite.sh>
2023-07-04 18:52:56 -0700

    sharedBitmap write to memory instead

--- cuda-by-example/sharedbitmap.js
+++ cuda-by-example/sharedbitmap.js
@@ -3,7 +3,7 @@ const dim = 1024
 export async function run () {
   const adapter = await navigator.gpu?.requestAdapter()
 
-  const device = await adapter?.requestDevice({ requiredFeatures: ['bgra8unorm-storage'] })
+  const device = await adapter?.requestDevice()
   if (!device) {
     throw Error('WebGPU not available')
   }
@@ -12,29 +12,23 @@ export async function run () {
   canvas.width = dim
   canvas.height = dim
 
-  const format = navigator.gpu.getPreferredCanvasFormat()
-  const ctx = canvas.getContext('webgpu')
-  ctx.configure({
-    device,
-    format,
-    usage: GPUTextureUsage.STORAGE_BINDING
-  })
+  const ctx = canvas.getContext('2d')
 
   const module = device.createShaderModule({
     code: `
-      @group(0) @binding(0) var texture: texture_storage_2d<bgra8unorm, write>;
+      @group(0) @binding(0) var<storage, read_write> data: array<u32>;
 
       var<workgroup> memory: array<array<f32, 16>, 16>;
   
       @compute @workgroup_size(16, 16)
-      fn draw(@builtin(global_invocation_id) id: vec3<u32>, @builtin(local_invocation_id) iid: vec3<u32>) {
+      fn draw(@builtin(global_invocation_id) id: vec3<u32>, @builtin(local_invocation_id) iid: vec3<u32>, @builtin(num_workgroups) dsize: vec3<u32>) {
         const pi = 3.1415926535897932f;
         const period = 128f;
 
         memory[iid.x][iid.y] = (sin(f32(id.x) * 2f * pi / period) + 1f) * (sin(f32(id.y) * 2f * pi / period) + 1f) / 4f;
         workgroupBarrier();
 
-        textureStore(texture, id.xy, vec4(0f, memory[15 - iid.x][15 - iid.y], 0f, 1.0f));
+        data[id.x + id.y * dsize.x * 16] = pack4x8unorm(vec4(0f, memory[15 - iid.x][15 - iid.y], 0f, 1.0f));
       }
       `
   })
@@ -44,9 +38,21 @@ export async function run () {
     compute: { module, entryPoint: 'draw' }
   })
 
+  const size = Uint32Array.BYTES_PER_ELEMENT * dim * dim
+
+  const workBuffer = device.createBuffer({
+    size,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
+  })
+
+  const resultBuffer = device.createBuffer({
+    size,
+    usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
+  })
+
   const bindGroup = device.createBindGroup({
     layout: pipeline.getBindGroupLayout(0),
-    entries: [{ binding: 0, resource: ctx.getCurrentTexture().createView() }]
+    entries: [{ binding: 0, resource: { buffer: workBuffer } }]
   })
 
   const encoder = device.createCommandEncoder()
@@ -56,8 +62,15 @@ export async function run () {
   pass.dispatchWorkgroups(dim / 16, dim / 16)
   pass.end()
 
+  encoder.copyBufferToBuffer(workBuffer, 0, resultBuffer, 0, size)
+
   const commandBuffer = encoder.finish()
   device.queue.submit([commandBuffer])
+
+  await resultBuffer.mapAsync(GPUMapMode.READ)
+  const result = new Uint8ClampedArray(resultBuffer.getMappedRange())
+  ctx.putImageData(new ImageData(result, dim, dim), 0, 0)
+  resultBuffer.unmap()
 
   return canvas
 }
